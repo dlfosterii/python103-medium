c8346eafbaf5e8daf1e60466dbedf9ce5ee17f36
David Foster <dlfosterii@yahoo.com>
2020-04-02 15:06:41 -0400

    cleaned up comments

--- tip_calc.py
+++ tip_calc.py
@@ -1,24 +1,11 @@
-#
-# 1. Tip Calculator
 # Your task is to write a program that calculates how much of a tip to leave at a restaurant.
-
-# Prompt the user for two things:
-
-# The total bill amount
-# The level of service, which can be one of the following: good, fair, or bad
-# Calculate the tip amount and the total amount (bill amount + tip amount). The tip percentage based
-#  on the level of service is based on:
-
-# good -> 20%
-# fair -> 15%
-# bad -> 10%
 
 #setup
 
 bill = int(input('Enter total of bill: '))
 service_level = input('How was the service? (good, fair, bad)')
 
-#code to make it happen
+#calculate tip amount
 
 tip = 0
 if service_level == 'good':
@@ -28,6 +15,7 @@ elif  service_level == 'fair':
 elif service_level == 'bad':
     tip += bill * .1
 
+#calculate bill total
 bill_total = tip + bill
 
 #final output
